d9be51ccdd9c99cfd152b1ae6079723ddfa58e45
Taylor Mock <tmock12@gmail.com>
2017-07-17 19:23:48 +0300

    Add hits and misses to Guess module

--- test/battleship_engine/guesses_test.exs
+++ test/battleship_engine/guesses_test.exs
@@ -1,9 +1,23 @@
 defmodule BattleshipEngine.GuessesTest do
   use ExUnit.Case, async: true
-  alias BattleshipEngine.Guesses
+  alias BattleshipEngine.{Coordinate, Guesses}
   doctest Guesses
 
   test "new returns mapset of hits and misses" do
     assert Guesses.new() == %Guesses{hits: MapSet.new(), misses: MapSet.new()}
+  end
+
+  def valid_coordinate, do: %Coordinate{row: 1, col: 1}
+
+  test "adding coordinates to hits" do
+    guesses = Guesses.add(Guesses.new(), :hit, valid_coordinate())
+
+    assert guesses.hits == MapSet.new([valid_coordinate()])
+  end
+
+  test "adding coordinates to misses" do
+    guesses = Guesses.add(Guesses.new(), :miss, valid_coordinate())
+
+    assert guesses.misses == MapSet.new([valid_coordinate()])
   end
 end
