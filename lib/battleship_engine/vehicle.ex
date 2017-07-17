c52e222dee8b3f49a71c08c32b6d6bef60e39aec
Taylor Mock <tmock12@gmail.com>
2017-07-17 19:22:57 +0300

    Add Vehicle module

---
+++ lib/battleship_engine/vehicle.ex
@@ -0,0 +1,39 @@
+defmodule BattleshipEngine.Vehicle do
+  alias BattleshipEngine.{Vehicle, Coordinate}
+  @enforce_keys [:coordinates, :hit_coordinates]
+  defstruct [:coordinates, :hit_coordinates]
+
+  def new(vehicle_name, %Coordinate{} = upper_left) do
+    with [_|_] = offsets <- offsets(vehicle_name),
+         %MapSet{} = coordinates <- add_coordinates(offsets, upper_left)
+    do
+      { :ok, %Vehicle{coordinates: coordinates, hit_coordinates: MapSet.new()} }
+    else
+      error -> error
+    end
+  end
+
+  defp offsets(:b52), do: [{0, 1}, {1, 1}, {1, 0}, {1, 2}, {2, 1}]
+  defp offsets(:stiletto), do: [{0, 0}, {0, 1}, {1, 0}, {1, 1}]
+  defp offsets(:battleship), do: [{0, 0}, {0, 1}, {0, 2}, {0, 3}]
+  defp offsets(:destroyer), do: [{0, 0}, {0, 1}, {0, 2}]
+  defp offsets(:higgins), do: [{0, 0}]
+  defp offsets(_), do: {:error, :invalid_vehicle_name}
+
+  defp add_coordinates(offsets, upper_left) do
+    Enum.reduce_while(offsets, MapSet.new(), fn offset, acc ->
+      add_coordinate(acc, upper_left, offset)
+    end)
+  end
+
+  defp add_coordinate(coordinates,
+                      %Coordinate{row: row, col: col},
+                      {offset_row, offset_col}) do
+    case Coordinate.new(row + offset_row, col + offset_col) do
+      {:ok, coordinate} ->
+        {:cont, MapSet.put(coordinates, coordinate)}
+      {:error, :invalid_coordinate} ->
+        {:halt, {:error, :invalid_coordinate}}
+    end
+  end
+end
---
+++ test/battleship_engine/vehicle_test.exs
@@ -0,0 +1,24 @@
+defmodule BattleshipEngine.VehicleTest do
+  use ExUnit.Case, async: true
+  alias BattleshipEngine.{Vehicle, Coordinate}
+  doctest Vehicle
+
+  defp new_vehicle(vehicle_name, row \\ 1, col \\ 1) do
+    Vehicle.new(vehicle_name, %Coordinate{row: row, col: col})
+  end
+
+  test "new with valid start returns hit coordinates and coordinates" do
+    {:ok, %Vehicle{} = vehicle} = new_vehicle(:b52)
+
+    assert vehicle.hit_coordinates == MapSet.new()
+    assert MapSet.size(vehicle.coordinates) == 5
+  end
+
+  test "new with invalid start returns invalid coordinate error" do
+    assert new_vehicle(:b52, 9, 9) == {:error, :invalid_coordinate}
+  end
+
+  test "new with invalid vehicle name returns invalid vehicle type" do
+    assert new_vehicle(:non_existant) == {:error, :invalid_vehicle_name}
+  end
+end
