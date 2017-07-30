08d5920fe62210a758d2a797f61018fe62d6d17a
Taylor Mock <tmock12@gmail.com>
2017-07-30 21:26:32 +0300

    Add ability to guess on board

--- lib/battleship_engine/board.ex
+++ lib/battleship_engine/board.ex
@@ -1,6 +1,12 @@
 defmodule BattleshipEngine.Board do
-  alias BattleshipEngine.Vehicle
+  alias BattleshipEngine.{Vehicle, Coordinate}
   def new(), do: %{}
+
+  def guess(board, %Coordinate{} = coordinate) do
+    board
+    |> check_all_vehicles(coordinate)
+    |> guess_response(board)
+  end
 
   def position_vehicle(board, key, %Vehicle{} = vehicle) do
     case overlaps_existing_vehicle?(board, key, vehicle) do
@@ -14,10 +20,50 @@ defmodule BattleshipEngine.Board do
     |> Enum.all?(&(Map.has_key?(board, &1)))
   end
 
+  defp check_all_vehicles(board, coordinate) do
+    Enum.find_value(board, :miss, fn {key, vehicle} ->
+      case Vehicle.guess(vehicle, coordinate) do
+        {:hit, vehicle} -> {key, vehicle}
+        :miss          -> false
+      end
+    end)
+  end
+
+  defp guess_response(:miss, board) do
+    {:miss, :none, :no_win, board}
+  end
+
+  defp guess_response({vehicle_key, vehicle}, board) do
+    {:hit, sunk_check?(board, vehicle_key), win_check?(board), %{board | vehicle_key => vehicle}}
+  end
+
   defp overlaps_existing_vehicle?(board, new_key, new_vehicle) do
     Enum.any?(board, fn {key, vehicle} ->
       key != new_key and Vehicle.overlaps?(vehicle, new_vehicle)
     end)
   end
 
+  defp win_check?(board) do
+    case all_sunk?(board) do
+      true -> :win
+      false -> :no_win
+    end
+  end
+
+  defp all_sunk?(board) do
+    Enum.all?(board, fn ({_k,v}) ->Vehicle.sunk?(v) end)
+  end
+
+  defp sunk_check?(board, key) do
+    case sunk?(board, key) do
+      true -> key
+      false -> :none
+    end
+  end
+
+  defp sunk?(board, key) do
+    board
+    |> Map.fetch!(key)
+    |> Vehicle.sunk?
+  end
 end
--- test/battleship_engine/board_test.exs
+++ test/battleship_engine/board_test.exs
@@ -23,4 +23,19 @@ defmodule BattleshipEngine.BoardTest do
     assert Board.all_vehicles_positioned?(full_board)
     refute Board.all_vehicles_positioned?(missing_vehicles)
   end
+
+  test "guess/2 when hit" do
+    coordinates = MapSet.new([%Coordinate{row: 1, col: 1}])
+    board = %{b52: %Vehicle{coordinates: coordinates, hit_coordinates: MapSet.new}}
+    hit_board = %{b52: %Vehicle{coordinates: coordinates, hit_coordinates: coordinates}}
+
+    assert Board.guess(board, %Coordinate{row: 1, col: 1}) == {:hit, :none, :no_win, hit_board}
+  end
+
+  test "guess/2 when miss" do
+    coordinates = MapSet.new([%Coordinate{row: 1, col: 1}])
+    board = %{b52: %Vehicle{coordinates: coordinates, hit_coordinates: MapSet.new}}
+
+    assert Board.guess(board, %Coordinate{row: 2, col: 3}) == {:miss, :none, :no_win, board}
+  end
 end
